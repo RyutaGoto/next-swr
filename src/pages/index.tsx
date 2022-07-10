f8dc5cd4c1b7b132549c7f10883b59fce422df92
RyutaGoto <kasa8gasa2rs6@gmail.com>
2022-07-10 17:52:06 +0900

    use hooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import type { NextPage } from 'next';
 import styles from '../styles/Home.module.css';
-import useSWR from 'swr';
-import { fetcherGet } from '../swr/fetcher';
+import { usePrefectures } from '../ hooks/usePrefectures';
 
 const Home: NextPage = () => {
-  const { data, error } = useSWR('/prefectures', fetcherGet);
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
-  return <div className={styles.container}>{JSON.stringify(data)}</div>;
+  const { prefectures, isLoading, isError } = usePrefectures();
+
+  if (isError) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+  return <div className={styles.container}>{JSON.stringify(prefectures)}</div>;
 };
 
 export default Home;
