d241bec9c6e4309f42bfc2031408bd6a359bd4df
RyutaGoto <kasa8gasa2rs6@gmail.com>
2022-07-12 00:55:58 +0900

    replaced hooks

--- src/hooks/usePrefectures.ts
+++ src/hooks/usePrefectures.ts
@@ -1,9 +1,13 @@
+import { useEffect } from 'react';
 import useSWR from 'swr';
 import { fetcherGet } from '../swr/fetcher';
 
 export const usePrefectures = () => {
   const { data, error } = useSWR('/prefectures', fetcherGet);
-
+  useEffect(() => {
+    console.log('data: ', data);
+    console.log('error: ', error);
+  }, [data, error]);
   return {
     prefectures: data,
     isLoading: !error && !data,
