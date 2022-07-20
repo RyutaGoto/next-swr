9c965ee4a64f88997f910313fb51f41c74378a2c
RyutaGoto <kasa8gasa2rs6@gmail.com>
2022-07-21 00:21:07 +0900

    added usePrefectures hooks

--- src/pages/prefectures.tsx
+++ src/pages/prefectures.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from 'next';
+import { usePrefectures } from '../hooks/usePrefectures';
 
 const Prefectures: NextPage = () => {
+  const { prefectures, isLoading, isError } = usePrefectures();
+
   return <></>;
 };
 
