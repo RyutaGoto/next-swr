1eba638ee0e7c25ab51dbda45e0291be4c06cd00
RyutaGoto <kasa8gasa2rs6@gmail.com>
2022-07-22 00:00:57 +0900

    fixed argument

--- src/hooks/usePrefectures.ts
+++ src/hooks/usePrefectures.ts
@@ -3,8 +3,7 @@ import useSWR from 'swr';
 import { fetcher } from '../swr/fetcher';
 
 export const usePrefectures = () => {
-  const token = `${process.env.NEXT_PUBLIC_RESAS_API_KEY}`;
-  const { data, error } = useSWR(['/prefectures', token], fetcher);
+  const { data, error } = useSWR('/prefectures', fetcher);
   useEffect(() => {
     console.log('data: ', data);
     console.log('error: ', error);
