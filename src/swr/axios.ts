c3a13450d0edc3bf373966761d148af4e1ad6fee
RyutaGoto <kasa8gasa2rs6@gmail.com>
2022-07-21 22:37:02 +0900

    insert token to axios instance

--- src/swr/axios.ts
+++ src/swr/axios.ts
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
+const token = `${process.env.NEXT_PUBLIC_RESAS_API_KEY}`;
+
 export const Axios = axios.create({
   baseURL: 'https://opendata.resas-portal.go.jp/api/v1',
   responseType: 'json',
   headers: {
     'Content-Type': 'application/json',
+    'X-API-KEY': `${token}`,
   },
 });
