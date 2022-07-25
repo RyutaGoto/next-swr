1dfca2e04e468ec64961ac8b276d576c47970b05
RyutaGoto <kasa8gasa2rs6@gmail.com>
2022-07-25 22:47:52 +0900

    fetch city data, display prefecturename

--- src/pages/prefectures/[code].tsx
+++ src/pages/prefectures/[code].tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from 'next';
+import { useCities } from '../../hooks/useCities';
+import { useRouter } from 'next/router';
 
 const City: NextPage = () => {
-  return <></>;
+  const router = useRouter();
+  const { cities, isError, isLoading } = useCities(router.query.code as string);
+  console.log('city: ', cities);
+  if (isError) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+  return (
+    <>
+      <h1>{router.query.name}の都市一覧</h1>
+    </>
+  );
 };
 
 export default City;
