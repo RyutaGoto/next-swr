db768abe438c6f68821ac68666ead18b092f6b26
RyutaGoto <kasa8gasa2rs6@gmail.com>
2022-07-26 23:34:04 +0900

    deleted console log

--- src/pages/prefectures/[code].tsx
+++ src/pages/prefectures/[code].tsx
@@ -5,7 +5,6 @@ import { useRouter } from 'next/router';
 const City: NextPage = () => {
   const router = useRouter();
   const { cities, isError, isLoading } = useCities(router.query.code as string);
-  console.log('city: ', cities);
   if (isError) return <div>failed to load</div>;
   if (isLoading) return <div>loading...</div>;
   return (
